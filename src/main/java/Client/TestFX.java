cae46579b36e81350e7f7815ff0b8fcd7998e7a1
MaedehBDNF <badanfirouzm@gmail.com>
2023-06-19 18:17:37 +0330

    - Add TestFX class - test loading start page

---
+++ src/main/java/Client/TestFX.java
@@ -0,0 +1,39 @@
+package Client;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class TestFX extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        // test all pages
+
+        testStart(stage);
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    private static void testStart(Stage stage) {
+        try {
+            Parent root = FXMLLoader.load(TestFX.class.getClassLoader().getResource("Start.fxml"));
+            Image icon = new Image("Images/SpotifyIcon.png");
+            stage.getIcons().add(icon);
+            stage.setTitle("Spotify");
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
