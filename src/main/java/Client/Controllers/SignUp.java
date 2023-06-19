a4f87cefdc216ab5e9eee8ebf182a2fbf64fa1af
Shaghayegh-Dianat <shaghayeghflower79@gmail.com>
2023-06-19 22:15:33 +0330

    define button,labels,TextField in SignUp controller

--- src/main/java/Client/Controllers/SignUp.java
+++ src/main/java/Client/Controllers/SignUp.java
@@ -1,4 +1,28 @@
 package Client.Controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.util.concurrent.Callable;
+
 public class SignUp {
+    @FXML
+    TextField usernameText,passwordText,emailText;
+
+    @FXML
+    Button SignUp,back;
+    @FXML
+    Label username,password,email,invalidEmail ;
+
+    public void SignUp() {
+
+    }
+
+    public void back() {
+
+    }
+
+
 }
