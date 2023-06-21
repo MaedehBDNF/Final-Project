34faaa8ff1b024934c98429a35371ec3b065810b
Shaghayegh-Dianat <shaghayeghflower79@gmail.com>
2023-06-21 10:34:03 +0330

    complete for presenting albums and complete search

---
+++ src/main/java/Client/Controllers/SearchPresentationController.java
@@ -0,0 +1,145 @@
+package Client.Controllers;
+
+import Client.ClientManager;
+import Client.LoadManager;
+import Shared.Dto.Search.SearchResponseDto;
+import Shared.Entities.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class SearchPresentationController implements Initializable {
+    private ClientManager client;
+    private LoadManager loader;
+    private SearchResponseDto responseDto;
+    private ArrayList<AlbumEntity> albums;
+    private Stage stage;
+
+    @FXML
+    private Label pageTitle;
+    @FXML
+    private Button back;
+    @FXML
+    private VBox results;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        if (this.responseDto != null) {
+            ArrayList<MusicEntity> musics = this.responseDto.getMusics();
+            if (musics != null) {
+                for (MusicEntity musicEntity : musics) {
+                    String buttonText = String.format("%-60s %-15s %-20s", musicEntity.getTitle(), musicEntity.getPopularity(), "Track");
+                    Button musicButton = new Button(buttonText);
+                    musicButton.setUserData(musicEntity);
+                    musicButton.setPrefHeight(30);
+                    musicButton.setPrefWidth(1000);
+                    musicButton.setOnAction(event -> {
+                        // todo 1. find one 2. load page
+                        System.out.println(musicButton.getText());
+                    });
+                    results.getChildren().add(musicButton);
+                }
+            }
+
+            ArrayList<AlbumEntity> albums = this.responseDto.getAlbums();
+            if (albums != null) {
+                for (AlbumEntity albumEntity : albums) {
+                    String buttonText = String.format("%-60s %-20s", albumEntity.getTitle(), "Album");
+                    Button albumButton = new Button(buttonText);
+                    albumButton.setUserData(albumEntity);
+                    albumButton.setPrefHeight(30);
+                    albumButton.setPrefWidth(1000);
+                    albumButton.setOnAction(event -> {
+                        System.out.println(albumButton.getText());
+                    });
+                    results.getChildren().add(albumButton);
+                }
+            }
+
+            ArrayList<PlaylistEntity> playlists = this.responseDto.getPlaylists();
+            if (playlists != null) {
+                for (PlaylistEntity playlistEntity : playlists) {
+                    String buttonText = String.format("%-60s %-15s %-20s", playlistEntity.getTitle(), playlistEntity.getPopularity(), "Playlist");
+                    Button playlistButton = new Button(buttonText);
+                    playlistButton.setUserData(playlistEntity);
+                    playlistButton.setOnAction(event -> {
+                        System.out.println(playlistButton.getText());
+                    });
+                    results.getChildren().add(playlistButton);
+                }
+            }
+
+            ArrayList<ArtistEntity> artists = this.responseDto.getArtists();
+            if (artists != null) {
+                for (ArtistEntity artistEntity : artists) {
+                    String buttonText = String.format("%-60s %-20s", artistEntity.getName(), "Artist");
+                    Button artistButton = new Button(buttonText);
+                    artistButton.setUserData(artistEntity);
+                    artistButton.setPrefHeight(30);
+                    artistButton.setPrefWidth(1000);
+                    artistButton.setOnAction(event -> {
+                        System.out.println(artistButton.getText());
+                    });
+                    results.getChildren().add(artistButton);
+                }
+            }
+
+            ArrayList<UserEntity> users = this.responseDto.getUsers();
+            if (users != null) {
+                for (UserEntity userEntity : users) {
+                    String buttonText = String.format("%-60s %-20s", userEntity.getUsername(), "User");
+                    Button userButton = new Button(buttonText);
+                    userButton.setUserData(userEntity);
+                    userButton.setPrefHeight(30);
+                    userButton.setPrefWidth(1000);
+                    userButton.setOnAction(event -> {
+                        System.out.println(userButton.getText());
+                    });
+                    results.getChildren().add(userButton);
+                }
+            }
+        }
+        if (this.albums != null) {
+            for (AlbumEntity albumEntity : this.albums) {
+                String buttonText = String.format("%-60s %-20s", albumEntity.getTitle(), "Album");
+                Button albumButton = new Button(buttonText);
+                albumButton.setUserData(albumEntity);
+                albumButton.setPrefHeight(30);
+                albumButton.setPrefWidth(1000);
+                albumButton.setOnAction(event -> {
+                    System.out.println(albumButton.getText());
+                });
+                results.getChildren().add(albumButton);
+            }
+        }
+    }
+
+    @FXML
+    public void back() {
+        this.stage = (Stage) this.back.getScene().getWindow();
+        this.loader.loadStartPage(this.stage);
+    }
+
+    public void setClient(ClientManager client) {
+        this.client = client;
+        this.loader = new LoadManager(this.client);
+    }
+
+    public void setResults(SearchResponseDto dto) {
+        this.responseDto = dto;
+    }
+
+    public void setAlbums(ArrayList<AlbumEntity> albums) {
+        this.albums = albums;
+    }
+
+
+}
